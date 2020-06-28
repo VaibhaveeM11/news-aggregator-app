ba38f1aa660cf7e7fd9352c6284c2d92630914a0
Vaibhavee Shinde <43693189+VaibhaveeM11@users.noreply.github.com>
2020-06-28 15:32:08 +0530

    updated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,39 @@
 
 // my Api key
-
 const apikey='680b52b3605346e096558d9b316f5ff0';
-
-// const fetchhere =document.querySelector('#news-articles');
+// news
 const searchTopic=document.querySelector('.search-container');
 const newsArea =document.getElementById('news-articles');
 const input = document.getElementById('search');
-
+//https://newsapi.org/v2/everything?q=${searchTopic}&apiKey=${apikey}
 searchTopic.addEventListener('submit',fetchNew);
-async function fetchNew(e)
+// Function to fetch news from NEWs api
+ function fetchNew(e)
 { 
    e.preventDefault();
    let searchTopic=input.value;
 
-   const url=`https://cors-escape.herokuapp.com/https://newsapi.org/v2/everything?q=${searchTopic}&apiKey=${apikey} `;
+   const url=`https://cors-proxy.htmldriven.com/?url=http://newsapi.org/v2/everything?q=bitcoin&from=2020-05-28&sortBy=publishedAt&apiKey=680b52b3605346e096558d9b316f5ff0
+   `;
 
-   console.log(searchTopic);
-  //fetching news
-  fetch(url,{mode:"no-cors"})
-  .then(res=>res.json())
-  .then(data=>console.log(data));
-    // const url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=[${api}]`;
+   console.log(searchTopic);  
+
+  fetch(url)    
+  .then((response)=>{ 
+      console.log('json');
+    return response.json();
+ })
+  .then((data)=>
+  {
+    console.log(data)
+  });
+   
+}
+
+
+ // const url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=[${api}]`;
 
 // function SearchValue( key='enter'|| )
 // {
 
 // }
-}
